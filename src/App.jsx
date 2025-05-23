00f239e290417fc0b83b14218ee8f3bf52ddc17f
Devank <singhdevank177@gmail.com>
2025-05-23 18:43:20 +0530

    Setting up socket.io

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
 const serverUrl = "http://localhost:8080";
 
 function App() {
-  const socket = io.connect(serverUrl, { secure: false });
+  const [socket, setSocket] = useState(null);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const newSocket = io(serverUrl, { secure: false });
+    setSocket(newSocket);
+  }, []);
+
+  const sendMsg = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit("message", message);
+      setMessage("");
+    }
+  };
+
+  useEffect(() => {
+    if (!socket) return;
+    
+    socket.on("message", (msg) => {
+      setMessages((prev) => [...prev, msg]);
+    });
+
+  }, [socket]);
 
   return (
     <>
       <h1 className="text-2xl">Connexus</h1>
+
+      <form action="">
+        <input
+          type="text"
+          className="border border-black"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={(e) => sendMsg(e)}>Send</button>
+      </form>
+
+      <ul>
+        {messages.map((msg, idx) => (
+          <li key={idx}>{msg}</li>
+        ))}
+      </ul>
     </>
   );
 }
